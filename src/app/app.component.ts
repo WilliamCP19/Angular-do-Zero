81952417f2d417a38b7088acf68f6bfee5fe459c
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-06-05 15:39:47 -0300

    Entendendo o 'If/Else' no Angular (ngIf)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 import { DirectivesComponent } from "./components/directives/directives.component";
+import { IfRenderComponent } from "./components/if-render/if-render.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent]
+    imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent]
 })
 export class AppComponent {
   userName = 'Joaquim';
