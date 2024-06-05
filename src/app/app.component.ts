0d356e2913caadc82a7b69891d7a88fcb112b117
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-06-05 11:58:28 -0300

    Aprendendo Diretivas (ngStyle e ngClass)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
+import { DirectivesComponent } from "./components/directives/directives.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent]
+    imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent]
 })
 export class AppComponent {
   userName = 'Joaquim';
