7f1f12994ca1d24b8549c44fbbdf98e6682b4ae4
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-06-19 18:13:54 -0300

    Aprendendo Eventos no Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import { FirstComponentComponent } from "./components/first-component/first-comp
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 import { DirectivesComponent } from "./components/directives/directives.component";
 import { IfRenderComponent } from "./components/if-render/if-render.component";
+import { EventosComponent } from "./components/eventos/eventos.component";
+import { EmitterComponent } from "./components/emitter/emitter.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent]
+    imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent, EventosComponent, EmitterComponent]
 })
 export class AppComponent {
   userName = 'Joaquim';
