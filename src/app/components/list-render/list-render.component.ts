5e43254ff465e3846907b52d51055d216a80a868
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-06-21 16:48:47 -0300

    Aprendendo Interfaces em Angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Animal } from '../../Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -8,11 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './list-render.component.html',
   styleUrl: './list-render.component.css'
 })
+
 export class ListRenderComponent {
-  animals = [
-    {name: "Turca", type: "Dog"},
-    {name: "Tom", type: "Cat"},
-    {name: "Frida", type: "Dog"},
-    {name: "Bob", type: "Horse"}
+  animals: Animal[] = [
+    { name: "Turca", type: "Dog", age: 10 },
+    { name: "Tom", type: "Cat", age: 4 },
+    { name: "Frida", type: "Dog", age: 1 },
+    { name: "Bob", type: "Horse", age: 7 }
   ]
+
+  animalDetails = "";
+
+  showAge (animal: Animal ) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age}`;
+    this.animalDetails += + animal.age == 1 ? " ano": " anos";
+  }
 }
