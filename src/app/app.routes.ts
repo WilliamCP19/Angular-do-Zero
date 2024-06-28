53ca39d45f14010f19415a523bf7ad08e495a582
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-06-28 11:55:21 -0300

    Capturando 1 animal da lista com '/list/:id'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 
 export const routes: Routes = [
     { path: '', component: FirstComponentComponent },
     { path: 'list', component:  ListRenderComponent },
-    { path: "change-number", component: EmitterComponent }
+    { path: 'list/:id', component: ItemDetailComponent },
+    { path: 'change-number', component: EmitterComponent }  
 ];
